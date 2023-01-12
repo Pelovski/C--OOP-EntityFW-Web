3bb36cb80cc740481a3c9d56d4b94b56581169fa
Pavel Pelovski <git.pelovski@gmail.com>
2023-01-12 12:41:26 +0100

    Ex.3 and Ex.4 completed

--- Data Structures/Stacks and Queues/P03. Maximum and Minimum Element/Program.cs
+++ Data Structures/Stacks and Queues/P03. Maximum and Minimum Element/Program.cs
@@ -2,6 +2,38 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n = int.Parse(Console.ReadLine());
+        var data = new Stack<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int command = int.Parse(input[0]);
+
+            if (command == 1)
+            {
+                data.Push(int.Parse(input[1]));
+            }
+
+            else if (command == 2)
+            {
+                data.Pop();
+            }
+            else if (command == 3)
+            {
+                Console.WriteLine(data.Max());
+
+            }
+
+            else
+            {
+                Console.WriteLine(data.Min());
+            }
+        }
+
+        string result = String.Join(", ", data.ToArray());
+
+        Console.WriteLine(result);
     }
 }
---
+++ Data Structures/Stacks and Queues/P04. Fast Food/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        int foodQuantity = int.Parse(Console.ReadLine());
+        string[] orderQuantity = Console.ReadLine().Split(' ');
+        var orders = new Queue<int>();
+
+        for (int i = 0; i < orderQuantity.Length; i++)
+        {
+            orders.Enqueue(int.Parse(orderQuantity[i]));
+        }
+
+        int biggestOrder = orders.Max();
+
+        while (foodQuantity > 0)
+        {
+            int currentOrder = orders.First();
+
+            if (foodQuantity > currentOrder)
+            {
+                orders.Dequeue();
+                foodQuantity -= currentOrder;
+                Console.WriteLine("Orders complete.");
+            }
+            else
+            {
+                break;
+            }
+
+
+        }
+
+
+        Console.Write("Orders left:{0}", String.Join(" ", orders.ToArray()));
+
+    }
+}
