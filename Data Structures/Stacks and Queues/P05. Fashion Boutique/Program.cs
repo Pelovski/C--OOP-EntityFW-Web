d300d84c14bc79893d7860e237ace828228a9265
Pavel Pelovski <git.pelovski@gmail.com>
2023-01-12 20:35:52 +0100

    ex05, 06 and 10 completed

--- Data Structures/Stacks and Queues/P05. Fashion Boutique/Program.cs
+++ Data Structures/Stacks and Queues/P05. Fashion Boutique/Program.cs
@@ -2,6 +2,36 @@
 {
     private static void Main(string[] args)
     {
-        
+        int[] inputClothes = Console.ReadLine()
+        .Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(item => int.Parse(item))
+        .ToArray();
+
+        int rackaCpacity = int.Parse(Console.ReadLine());
+        var clothes = new Stack<int>(inputClothes);
+
+
+        int colothesLength = clothes.Count;
+        int racksCount = 1;
+        int currentCapacity = rackaCpacity;
+
+        for (int i = 0; i < colothesLength; i++)
+        {
+            int currentClothe = clothes.Pop();
+
+            if (currentCapacity >= currentClothe)
+            {
+                currentCapacity -= currentClothe;
+            }
+            else
+            {
+                currentCapacity = rackaCpacity - currentClothe;
+                racksCount++;
+            }
+        }
+
+        Console.WriteLine(racksCount);
+
+
     }
 }
---
+++ Data Structures/Stacks and Queues/P10. Crossroads/Program.cs
@@ -0,0 +1,66 @@
+﻿ class Program
+{
+    private static void Main(string[] args)
+    {
+        int greenLightDuration = int.Parse(Console.ReadLine());
+        int freewindowDuration = int.Parse(Console.ReadLine());
+
+        int duration = greenLightDuration + freewindowDuration;
+        int carsCount = 0;
+
+        var cars = new Queue<string>();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            int currnetGreen = greenLightDuration;
+            int currentDuration = freewindowDuration;
+            
+
+            if (input == "END")
+            {
+                break;
+            }
+
+            if (input != "green")
+            {
+                cars.Enqueue(input);
+            }
+            else
+            {
+                int carsLength = cars.Count();
+
+                for (int i = 0; i < carsLength; i++)
+                {
+                    string currentCar = cars.First();
+
+                    if (currnetGreen > 0)
+                    {
+                        currnetGreen -= currentCar.Length;
+                        currentDuration = currnetGreen + freewindowDuration;
+
+                        if (currentDuration < 0)
+                        {
+
+                            char hittedChar = currentCar[currentCar.Length + currentDuration];
+
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{currentCar} was hit at {hittedChar}.");
+                            return;
+                        }
+
+                        carsCount++;
+                        cars.Dequeue();
+                    }
+
+                }
+            }
+
+        }
+
+        Console.WriteLine("Everyone is safe.");
+        Console.WriteLine($"{carsCount} total cars passed the crossroads.");
+
+    }
+}
