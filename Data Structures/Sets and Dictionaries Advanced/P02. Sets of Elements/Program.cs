ccb35dc9192565685634a7379bc029cb924bcdaa
Pavel Pelovski <git.pelovski@gmail.com>
2023-01-13 13:55:01 +0100

    Ex.1 und 2 completed

---
+++ Data Structures/Sets and Dictionaries Advanced/P02. Sets of Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        int firstHashCount = input[0];
+        int secondHashCount = input[1];
+
+        var firstCollection = new HashSet<string>();
+        var secondCollection = new HashSet<string>();
+
+        AddToCollection(firstCollection, firstHashCount);
+        AddToCollection(secondCollection, secondHashCount);
+
+        var result = new HashSet<string>(firstCollection);
+
+        result.IntersectWith(secondCollection);
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+
+    public static HashSet<string> AddToCollection(HashSet<string> collection, int countIteration)
+    {
+        for (int i = 0; i < countIteration; i++)
+        {
+           string input = Console.ReadLine(); ;
+            collection.Add(input);
+        }
+        return collection;
+    }
+
+}
