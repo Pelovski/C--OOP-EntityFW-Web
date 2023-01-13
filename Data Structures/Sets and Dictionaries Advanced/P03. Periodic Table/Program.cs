b4f6ef7e42c2dad8fa4168843cdad48168fb0f02
Pavel Pelovski <git.pelovski@gmail.com>
2023-01-13 19:11:22 +0100

    ex.3 completed

---
+++ Data Structures/Sets and Dictionaries Advanced/P03. Periodic Table/Program.cs
@@ -0,0 +1,22 @@
+﻿ class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var data = new SortedSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                data.Add(input[j]);
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", data));
+    }
+}
