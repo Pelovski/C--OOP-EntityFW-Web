1187b096fe4ce7f065e77af2480d0dab0f080f5a
Pavel Pelovski <git.pelovski@gmail.com>
2023-01-13 12:54:05 +0100

    Sets and Dictionaries exercies created / Ex.1 completed

---
+++ Data Structures/Sets and Dictionaries Advanced/P01. Unique Usernames/Program.cs
@@ -0,0 +1,17 @@
+﻿ class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var data = new HashSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+
+            data.Add(input);
+        }
+
+        Console.WriteLine(String.Join("\n", data));
+    }
+}
