a7280b97aec950ac040067638bade6e5efb63a31
Pavel Pelovski <git.pelovski@gmail.com>
2023-01-15 19:44:29 +0100

    Ex 4 completed

--- Data Structures/Sets and Dictionaries Advanced/P04. Even Times/Program.cs
+++ Data Structures/Sets and Dictionaries Advanced/P04. Even Times/Program.cs
@@ -1,7 +1,31 @@
-﻿ class Program
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
 {
     private static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+
+        var data = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (!data.ContainsKey(input))
+            {
+                data.Add(input, 1);
+            }
+
+            data[input]++;
+
+            
+        }
+
+        foreach (var item in data)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
     }
 }
