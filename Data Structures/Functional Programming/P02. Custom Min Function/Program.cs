e97ea5d9185d491276258b2362452d453acfc7fd
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-06 22:22:52 +0100

    Ex.2 completed

---
+++ Data Structures/Functional Programming/P02. Custom Min Function/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Func<int[], int> func =  x => x.Min();
+
+        int[] input = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+
+        Console.WriteLine(func(input));
+    }
+}
