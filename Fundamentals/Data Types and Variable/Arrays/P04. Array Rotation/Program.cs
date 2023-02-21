526b29548bf6e296aacdb021586ac8a5b6c305eb
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-21 18:19:14 +0100

    Ex.4 completed

---
+++ Fundamentals/Data Types and Variable/Arrays/P04. Array Rotation/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+
+        int rotation = int.Parse(Console.ReadLine());
+
+      
+
+        for (int i = 0; i < rotation; i++)
+        {
+            int firstNumber = input[0];
+
+            for (int j = 0; j < input.Length -1; j++)
+            {
+                input[j] = input[j + 1];
+            }
+
+            input[input.Length -1] = firstNumber;
+        }
+
+        Console.WriteLine(string.Join(" ", input));
+    }
+}
