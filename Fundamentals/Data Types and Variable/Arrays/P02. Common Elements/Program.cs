3554f67ee51b55f769ade8f6b4db693af22f4458
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-21 17:15:39 +0100

    Ex. 2 and 3 completed

---
+++ Fundamentals/Data Types and Variable/Arrays/P02. Common Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] firstSentence = Console.ReadLine().Split(" ");
+        string[] secondSentence = Console.ReadLine().Split(" ");
+
+        string result = "";
+
+            for (int i = 0; i < secondSentence.Length; i++)
+        {
+            for (int j = 0; j < firstSentence.Length; j++)
+            {
+                if (firstSentence[j] == secondSentence[i])
+                {
+                    result += secondSentence[i] + " ";
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+
+    }
+}
---
+++ Fundamentals/Data Types and Variable/Arrays/P03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n  = int.Parse(Console.ReadLine());
+
+        int[] firstArr = new int[n];
+        int[] secountArr = new int[n];
+
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            if (i % 2 == 0)
+            {
+                firstArr[i] = input[0];
+                secountArr[i] = input[1];
+            }
+            else
+            {
+                secountArr[i] = input[0];
+                firstArr[i] = input[1];
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", firstArr));
+        Console.WriteLine(string.Join(" ", secountArr));
+
+    }
+}
