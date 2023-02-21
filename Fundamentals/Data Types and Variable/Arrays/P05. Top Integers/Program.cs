07e79c5142e83240695a0f8e709ce0c9201167b1
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-21 23:16:23 +0100

    Ex.5 completed

--- Fundamentals/Data Types and Variable/Arrays/P05. Top Integers/Program.cs
+++ Fundamentals/Data Types and Variable/Arrays/P05. Top Integers/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+
+        string topNumbers = "";
+  
+        for (int i = 0; i < input.Length; i++)
+        {
+            int counter = 0;
+            for (int j = i; j < input.Length; j++)
+            {
+                if (j != input.Length -1 && input[i] > input[j + 1])
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == input.Length -1 -i)
+            {
+                topNumbers += input[i] + " ";
+            }
+        }
+
+        Console.WriteLine(topNumbers);
+    }
+}
