d9dde0784d964dad49160e82564fb2cff50a90d2
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-23 20:06:00 +0100

    Ex.7 and 8 completed

---
+++ Fundamentals/Data Types and Variable/Arrays/P07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int number = 0;
+        int count = 1;
+        int currentCount = 1;
+
+        for (int i = 0; i < numbers.Length -1; i++)
+        {
+            int element = numbers[i];
+            int nextElement = numbers[i + 1];
+
+            if (element == nextElement)
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentCount = 1;
+            }
+
+            if (currentCount > count)
+            {
+                count = currentCount;
+                number = element;
+            }
+
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(number + " ");
+        }
+
+        
+
+       
+    }
+}
---
+++ Fundamentals/Data Types and Variable/Arrays/P08.Magic Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                int currentNum = input[i] + input[j];
+
+                if (currentNum == number)
+                {
+                    Console.WriteLine($"{input[i]} {input[j]}");
+                    break;
+                }
+            }
+        }
+    }
+}
