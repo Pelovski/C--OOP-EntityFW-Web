a73ddb6bc41cc93faaae76601f0ca0b458c51878
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-24 20:31:48 +0100

    New project created and Ex.1 und 2 completed

---
+++ Fundamentals/Objects and Classes/P01. Advertisement Message/Article.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace P01._Advertisement_Message
+{
+    public  class Article
+    {
+
+        public Article(string titel, string content, string author)
+        {
+            this.Title= titel;
+            this.Content= content;
+            this.Author= author;
+        }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public void EditContent(string newContent) 
+        {
+            this.Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            this.Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            this.Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
+        }
+    }
+}
---
+++ Fundamentals/Objects and Classes/P01. Advertisement Message/Program.cs
@@ -0,0 +1,40 @@
+﻿using P01._Advertisement_Message;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+        int n = int.Parse(Console.ReadLine());
+
+        string title = input[0];
+        string content = input[1];
+        string author = input[2];
+
+        var article = new Article(title, content, author);
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] command = Console.ReadLine()
+                .Split(": ");
+
+            if (command[0] == "Edit")
+            {
+                article.EditContent(command[1]);
+            }
+            else if(command[0] == "ChangeAuthor")
+            {
+                article.ChangeAuthor(command[1]);
+            }
+
+            else if (command[0] == "Rename")
+            {
+                article.Rename(command[1]);
+            }
+        }
+
+        Console.WriteLine(article.ToString());
+    }
+}
