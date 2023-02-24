a07e28e8667a8238796c668cf0e91db54ebd2a0d
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-24 08:44:05 +0100

    Ex. 2 and 3 completed

---
+++ Fundamentals/Methods/P02. Center Point/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        Tribonacci(number);
+    }
+
+    private static void Tribonacci(int number)
+    {
+        int[] arr = new int[number];
+
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i < number; i++)
+        {
+            arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+        }
+
+        Console.WriteLine(string.Join(" ", arr));
+    }
+}
---
+++ Fundamentals/Methods/P03. Multiplication Sign/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        Sign(firstNumber,secondNumber,thirdNumber);
+
+    }
+
+    public static void Sign (int firstNumber, int secondNumber, int thirdNumber)
+    {
+        string isNegative = firstNumber < 0 || secondNumber < 0 || thirdNumber < 0 ? "negative" : "positive";
+
+        Console.WriteLine(isNegative);
+    }
+}
