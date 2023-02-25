c3ba39387ced7c58565d4d868513acbf6f832fcc
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-25 14:15:01 +0100

    Ex.4 completed

---
+++ Fundamentals/Objects and Classes/P04. Students/Program.cs
@@ -0,0 +1,29 @@
+﻿using P04._Students;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int studentsCount = int.Parse(Console.ReadLine());
+        var students = new List<Student>();
+
+        for (int i = 0; i < studentsCount; i++)
+        {
+            string[] inputStudents = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string firstName = inputStudents[0];
+            string lastName = inputStudents[1];
+            double grade = double.Parse(inputStudents[2]);
+
+            var student = new Student(firstName, lastName, grade);
+
+            students.Add(student);
+        }
+
+        foreach (var student in students.OrderByDescending(x => x.Grade))
+        {
+            Console.WriteLine(student.ToString());
+        }
+    }
+}
---
+++ Fundamentals/Objects and Classes/P04. Students/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace P04._Students
+{
+    public  class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+        }
+    }
+}
