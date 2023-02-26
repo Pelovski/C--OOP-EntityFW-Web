6793dad3e534c5aaf50cb934c6f91dd115d7c7df
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-26 12:42:46 +0100

    New project created / Ex.1 and Ex.2 completed

---
+++ Fundamentals/Associative Arrays/P01. Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        var data = new Dictionary<char, int>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            char[] letters = input[i].ToCharArray();
+
+            for (int j = 0; j < letters.Length; j++)
+            {
+                char letter = letters[j];
+
+                if (!data.ContainsKey(letter))
+                {
+                    data.Add(letter, 0);
+                }
+
+                    data[letter]++;
+
+            }
+        }
+
+        foreach (var letter in data)
+        {
+            Console.WriteLine($"{letter.Key} -> {letter.Value}");
+        }
+    }
+}
---
+++ Fundamentals/Associative Arrays/P02. A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var data = new Dictionary<string, int>();
+
+        while (true)
+        {
+            string resource = Console.ReadLine();
+
+            if (resource == "stop")
+            {
+                break;
+            }
+
+            int quantity = int.Parse(Console.ReadLine());
+
+            if (!data.ContainsKey(resource))
+            {
+                data.Add(resource, 0);
+            }
+
+            data[resource] += quantity;
+
+        }
+
+        foreach (var resource in data)
+        {
+            Console.WriteLine($"{resource.Key} -> {resource.Value}");
+        }
+    }
+}
