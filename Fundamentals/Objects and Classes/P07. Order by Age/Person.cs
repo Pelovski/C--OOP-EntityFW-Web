6fe4c3f63e35b5e9cb1b82764a7a69060f121a01
Pavel Pelovski <git.pelovski@gmail.com>
2023-02-26 11:40:21 +0100

    Ex.7 completed

---
+++ Fundamentals/Objects and Classes/P07. Order by Age/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace P07._Order_by_Age
+{
+    public class Person
+    {
+
+        public Person(string name, string iD, int age)
+        {
+            this.Name = name;
+            this.ID = iD;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public string ID { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ Fundamentals/Objects and Classes/P07. Order by Age/Program.cs
@@ -0,0 +1,32 @@
+﻿using P07._Order_by_Age;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var persons = new List<Person>();
+        while (true)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            if (input[0] == "End")
+            {
+                break;
+            }
+
+            string name = input[0];
+            string id = input[1];
+            int age = int.Parse(input[2]);
+
+            var person = new Person(name, id, age);
+
+            persons.Add(person);
+        }
+
+        foreach (var person in persons.OrderBy(x => x.Age))
+        {
+            Console.WriteLine($"{person.Name} with ID: {person.ID} is {person.Age} years old.");
+        }
+    }
+}
