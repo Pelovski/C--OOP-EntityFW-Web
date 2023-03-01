c86c04f3950298347d9f001f3ab0e14e154f8ca3
Pavel Pelovski <git.pelovski@gmail.com>
2023-03-01 15:56:40 +0100

    New project created and Ex.1 completed

---
+++ Fundamentals/Strings and Text Processing/P01. Valid Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+        var result = new List<string>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i].Length >= 3 && input[i].Length <=16)
+            {
+                if (input[i].All(c => char.IsLetterOrDigit(c) || c == '-' || c == '!'))
+                {
+                    result.Add(input[i]);
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join("\n", result));
+    }
+}
