1c23812f0175b4593f3b430657b0004bad34371c
Pavel Pelovski <git.pelovski@gmail.com>
2023-03-02 23:45:48 +0100

    Ex.2 Completed

---
+++ Fundamentals/Strings and Text Processing/P02. Character Multiplier/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        char[] firstWord = input[0].ToCharArray();
+        char[] secondWord = input[1].ToCharArray();
+
+        int maxLength = Math.Max(firstWord.Length, secondWord.Length);
+        int minLenght = Math.Min(firstWord.Length, secondWord.Length);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < minLenght; i++)
+        {
+
+            sum += firstWord[i] * secondWord[i];
+            count++;
+        }
+
+        if (count != maxLength)
+        {
+            var n = maxLength - minLenght;
+           
+        }
+
+        Console.WriteLine(sum);
+    }
+}
