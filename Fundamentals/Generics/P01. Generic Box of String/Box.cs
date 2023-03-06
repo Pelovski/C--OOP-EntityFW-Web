d1f2226787b23f6eb52ad53fcb9f81a08a1c9eab
Pavel Pelovski <git.pelovski@gmail.com>
2023-03-06 20:36:03 +0100

    Ex.1 completed

---
+++ Fundamentals/Generics/P01. Generic Box of String/Box.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace P01._Generic_Box_of_String
+{
+    internal class Box<T>
+    {
+        public Box(string text)
+        {
+            this.Text = text;
+        }
+
+        public string Text { get; set; }
+        public override string ToString()
+        {
+            return $"{this.GetType().GetGenericArguments()[0]}: {this.Text}";
+        }
+    }
+}
--- Fundamentals/Generics/P01. Generic Box of String/Program.cs
+++ Fundamentals/Generics/P01. Generic Box of String/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+using P01._Generic_Box_of_String;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n = int.Parse(Console.ReadLine());
+
+        var data = new List<Box<string>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+
+            var box = new Box<string>(input);
+
+            data.Add(box);
+
+        }
+
+        foreach (var item in data)
+        {
+            Console.WriteLine(item.ToString());
+        }
     }
 }
