107977e8c10c99c46d36feaeb4fd7115e8d77ed6
Pavel Pelovski <git.pelovski@gmail.com>
2023-03-06 20:55:48 +0100

    Ex.2 completed

---
+++ Fundamentals/Generics/P02. Generic Box of Integer/Box.cs
@@ -0,0 +1,16 @@
+﻿namespace P02._Generic_Box_of_Integer
+{
+    internal class Box<T>
+    {
+        public Box(int number)
+        {
+            this.Number = number;
+        }
+
+        public int Number { get; set; }
+        public override string ToString()
+        {
+            return $"{this.GetType().GetGenericArguments()[0]}: {this.Number}";
+        }
+    }
+}
---
+++ Fundamentals/Generics/P02. Generic Box of Integer/Program.cs
@@ -0,0 +1,24 @@
+﻿using P02._Generic_Box_of_Integer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var data = new List<Box<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            var box = new Box<int>(input);
+
+            data.Add(box);
+        }
+
+        foreach (var item in data)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
