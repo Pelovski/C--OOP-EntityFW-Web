9168dcad2755eced17459027155079b832a6a769
Pavel Pelovski <git.pelovski@gmail.com>
2023-03-10 19:37:17 +0100

    Ex.3 completed

---
+++ Fundamentals/Generics/P03. Generic Swap Method Strings/Box.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace P03._Generic_Swap_Method_Strings
+{
+    internal class Box<T>
+    {
+        public Box(List<T> data)
+        {
+            this.Values = data;
+        }
+        public List<T> Values { get; set; }
+
+        public void Swap(List<T> data, int firstIndex, int secondIntex)
+        {
+            T templateValue = data[firstIndex];
+
+            data[firstIndex] = data[secondIntex];
+            data[secondIntex] = templateValue;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder= new StringBuilder();
+            foreach (var item in this.Values)
+            {
+                stringBuilder.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
--- Fundamentals/Generics/P03. Generic Swap Method Strings/Program.cs
+++ Fundamentals/Generics/P03. Generic Swap Method Strings/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using P03._Generic_Swap_Method_Strings;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var data =  new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+
+            data.Add(input);
+        }
+
+        var box = new Box<string>(data);
+
+        int[] indexToSwap = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+
+        box.Swap(box.Values, indexToSwap[0], indexToSwap[1]);
+
+        Console.WriteLine(box);
+    }
+
+}
