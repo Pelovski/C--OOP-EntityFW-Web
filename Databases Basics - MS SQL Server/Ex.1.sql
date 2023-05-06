edf1ac037ea4dfc3cd8a7ee0692ef57be164ba9e
Pavel Pelovski <git.pelovski@gmail.com>
2023-05-06 17:42:21 +0200

    Ex.2 completed

--- Databases Basics - MS SQL Server/Ex.1.sql
+++ Databases Basics - MS SQL Server/Ex.1.sql
@@ -11,3 +11,24 @@ CREATE TABLE Towns(
 
 ALTER TABLE Minions
 ADD TownId INT FOREIGN KEY REFERENCES Towns(Id)
+
+INSERT INTO Towns(Id, [Name])
+    VALUES 
+		   (1, 'Sofia'),
+		   (2, 'Plovdiv'),
+		   (3, 'Varna')
+
+
+INSERT INTO Minions (Id, [Name], Age, TownId)
+	VALUES
+		   (1, 'Kevin', 22, 1),
+		   (2, 'Bob', 15, 3),
+		   (3, 'Steward',NULL, 2)
+
+
+TRUNCATE TABLE Minions
+
+SELECT * FROM Minions
+
+DROP TABLE Minions
+DROP TABLE Towns
