66b57848cea899b347160606edd3e85370d63ded
Pavel Pelovski <git.pelovski@gmail.com>
2023-05-14 21:14:24 +0200

    From Ex.5 to Ex.10 completed

--- Databases Basics - MS SQL Server/CRUD.sql
+++ Databases Basics - MS SQL Server/CRUD.sql
@@ -16,4 +16,26 @@ SELECT FirstName, MiddleName, LastName FROM  Employees
 
 --Find Email Address of Each Employee
 
-SELECT FirstName + '.' + LastName  + '@softuni.bg' AS [Full Email Address] FROM  Employees 
+SELECT FirstName + '.' + LastName  + '@softuni.bg' AS [Full Email Address] FROM  Employees
+
+--Find All Different Employee’s Salaries
+SELECT DISTINCT Salary FROM Employees
+
+--Find all Information About Employees
+
+SELECT * FROM Employees
+WHERE JobTitle = 'Sales Representative'
+
+
+-- Find Names of All Employees by Salary in Range
+
+SELECT FirstName, LastName, JobTitle FROM Employees
+WHERE Salary >= 20000 AND Salary <= 30000
+
+-- Find Names of All Employees 
+
+SELECT FirstName + ' ' + MiddleName + ' ' + LastName AS [Full Name] FROM Employees
+WHERE Salary = 25000 OR Salary = 14000 OR Salary = 12500 OR Salary = 23600
+
+-- Find All Employees Without Manager
+
