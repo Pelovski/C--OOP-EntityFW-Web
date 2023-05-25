4eed4ba71b9f6a3808d6609e9944ddf313b6e76a
Pavel Pelovski <git.pelovski@gmail.com>
2023-05-25 18:58:53 +0200

    Connection string and DB sets implemented

--- Entity Framework/Football Betting/FootballBetting/P03_FootballBetting.Data/FootballBettingContext.cs
+++ Entity Framework/Football Betting/FootballBetting/P03_FootballBetting.Data/FootballBettingContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data.Models;
 
 namespace P03_FootballBetting.Data
 {
@@ -9,6 +10,45 @@ namespace P03_FootballBetting.Data
 
         }
 
+        public FootballBettingContext(DbContextOptions options)
+            : base(options)
+        {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Bet> Bets { get; set; }
+
+        public DbSet<Color> Colors { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Player> Players { get; set; }
+
+        public DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+
+        public DbSet<Position> Positions { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Town> Towns { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
