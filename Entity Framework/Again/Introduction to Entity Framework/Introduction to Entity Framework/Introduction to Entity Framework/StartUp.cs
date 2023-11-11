56fd8d4272b131d6d852e583d7a7e5588d4e43c8
Pavel Pelovski <git.pelovski@gmail.com>
2023-11-11 22:00:22 +0100

    Nugets installed and database config

---
+++ Entity Framework/Again/Introduction to Entity Framework/Introduction to Entity Framework/Introduction to Entity Framework/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using P01._Import_the_SoftUni_Database.Data;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        var dbContext = new SoftUniContext();
+
+        foreach (var db in dbContext.Addresses)
+        {
+            Console.WriteLine(db.AddressText.ToString());
+        }
+    }
+}
