f8d9dd726a35a6e6647fee27e74e551f76c3d65b
Pavel Pelovski <git.pelovski@gmail.com>
2023-11-15 21:56:12 +0100

    Ex.1 completed

--- Entity Framework/Again/Advanced Querying/BookShop/StartUp.cs
+++ Entity Framework/Again/Advanced Querying/BookShop/StartUp.cs
@@ -1,5 +1,10 @@
 ﻿namespace BookShop
 {
+    using System;
+    using System.Linq;
+    using System.Text;
+    using BookShop.Models;
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
 
@@ -8,7 +13,39 @@
         public static void Main()
         {
             using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+
+            //DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine().ToLower();
+
+             var result = GetBooksByAgeRestriction(db, command);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var sb =  new StringBuilder();
+
+
+            var books = context
+                .Books
+                .AsEnumerable()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command)
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(b => b.Title)
+                .ToList();
+
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
