2cd23b896adae374497421f9375f2beee14db935
Pavel Pelovski <git.pelovski@gmail.com>
2023-11-15 20:27:20 +0100

    DbSets created and models done

--- Entity Framework/Again/Football Betting/P03_FootballBetting.Data/FootballBettingContext.cs
+++ Entity Framework/Again/Football Betting/P03_FootballBetting.Data/FootballBettingContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using P03_FootballBetting.Data.Models;
 
 namespace P03_FootballBetting.Data
 {
@@ -13,6 +14,28 @@ namespace P03_FootballBetting.Data
         {
         }
 
+
+        public virtual DbSet<Bet> Bets { get; set; }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<Team> Teams { get; set; }
+
+        public virtual DbSet<Town> Towns { get; set; }
+
+        public virtual DbSet<Game> Games { get; set; }
+
+        public virtual DbSet<Color> Colors { get; set; }
+
+        public virtual DbSet<Player> Players { get; set; }
+
+        public virtual DbSet<Country> Countries { get; set; }
+
+        public virtual DbSet<Position> Positions { get; set; }
+
+        public virtual DbSet<PlayerStatistic> PlayerStatistics { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=Arsenal;Database=FootballBetting;Integrated Security=true;");
@@ -20,7 +43,7 @@ namespace P03_FootballBetting.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(modelBuilder); 
         }
     }
 }
--- Entity Framework/Again/Football Betting/P03_FootballBetting/StartUp.cs
+++ Entity Framework/Again/Football Betting/P03_FootballBetting/StartUp.cs
@@ -1,7 +1,11 @@
-﻿internal class StartUp
+﻿using P03_FootballBetting.Data;
+
+internal class StartUp
 {
     private static void Main(string[] args)
     {
-        
+        var context = new FootballBettingContext();
+
+        context.Database.EnsureCreated();
     }
 }
