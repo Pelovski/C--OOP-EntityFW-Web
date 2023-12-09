d1a2d24e4aae33b6630418c256d87f1c2cab1a01
Pavel Pelovski <git.pelovski@gmail.com>
2023-12-09 16:51:18 +0100

    implementet RealEstate.Importet

--- Entity Framework/Again/Real Estate Property Ads/Real Estate Property Ads/RealEstates.Importer/Program.cs
+++ Entity Framework/Again/Real Estate Property Ads/Real Estate Property Ads/RealEstates.Importer/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System.Text.Json;
+using RealEstates.Data;
+using RealEstates.Importer;
+using RealEstates.Services;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       var json = File.ReadAllText("imot.bg-raw-data-2020-07-23.json");
+
+        var properties = JsonSerializer.Deserialize<IEnumerable<JsonRealEstateProperty>>(json);
+
+        var db = new RealEstateDbContext();
+
+        IPropertyService propertyService = new PropertyService(db);
+
+        foreach (var property in properties.Where(x => x.Price > 1000))
+        {
+            propertyService.Create(
+                property.Size,
+                property.District,
+                property.Type, 
+                property.BuildingType,
+                property.Year,
+                property.Price,
+                property.Floor,
+                property.TotalFloors);
+        }
     }
-}
+}   
